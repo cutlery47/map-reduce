54d0ed342fe2b3e8a9c0366a034e4c99f3534faf
Arkhip McNutt <ortiz47@mail.ru>
2025-03-19 12:24:33 +0300

    fix: config fix

--- mapreduce/config.go
+++ mapreduce/config.go
@@ -8,49 +8,47 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// ================== WORKER CONFIG ==================
-
 type Config struct {
 	// ================== GENERAL ==================
 	// file to be processed
-	File string `env:"FILE" env-required:"true"`
+	File string `env:"FILE" env-required:""`
 	// amount of workers
-	Mappers int `env:"MAPPERS" env-required:"true"`
+	Mappers int `env:"MAPPERS" env-required:""`
 	// amount of reducers
-	Reducers int `env:"REDUCERS" env-required:"true"`
+	Reducers int `env:"REDUCERS" env-required:""`
 	// transport to be used in the system
-	Transport string `env:"TRANSPORT" env-required:"true"`
+	Transport string `env:"TRANSPORT" env-required:""`
 	// directory to map into master node
-	MappedDir string `env:"MAPPED_DIRECTORY" env-required:"false" env-default:"data"`
+	MappedDir string `env:"MAPPED_DIRECTORY" env-default:"data"`
 	// time for all worker nodes to register on master
-	RegisterDur time.Duration `env:"REGISTER_DURATION" env-required:"false" env-default:"30s"`
+	RegisterDur time.Duration `env:"REGISTER_DURATION" env-default:"30s"`
 
 	// ================== MASTER ==================
-	MasterHost            string        `env:"MASTER_HOST"                   env-required:"false" env-default:"localhost"`
-	MasterPort            string        `env:"MASTER_PORT"                   env-required:"false" env-default:"8080"`
-	MasterReadTimeout     time.Duration `env:"MASTER_READ_TIMEOUT"           env-required:"false" env-default:"3s"`
-	MasterWriteTimeout    time.Duration `env:"MASTER_WRITE_TIMEOUT"          env-required:"false" env-default:"3s"`
-	MasterShutdownTimeout time.Duration `env:"MASTER_SHUTDOWN_TIMEOUT"       env-required:"false" env-default:"0s"`
+	MasterHost            string        `env:"MASTER_HOST"              env-default:"localhost"`
+	MasterPort            string        `env:"MASTER_PORT"              env-default:"8080"`
+	MasterReadTimeout     time.Duration `env:"MASTER_READ_TIMEOUT"      env-default:"3s"`
+	MasterWriteTimeout    time.Duration `env:"MASTER_WRITE_TIMEOUT"     env-default:"3s"`
+	MasterShutdownTimeout time.Duration `env:"MASTER_SHUTDOWN_TIMEOUT"  env-default:"3s"`
 	// time for master to receive request from any worker
-	MasterRequestAwaitDur time.Duration `env:"MASTER_REQUEST_AWAIT_DURATION" env-required:"false" env-default:"30s"`
+	MasterRequestAwaitDur time.Duration `env:"MASTER_REQUEST_AWAIT_DURATION" env-default:"10s"`
 
 	// ================== WORKER ==================
-	WorkerHost            string        `env:"WORKER_HOST"              env-required:"false" env-default:"localhost"`
-	WorkerReadTimeout     time.Duration `env:"WORKER_READ_TIMEOUT"      env-required:"false" env-default:"3s"`
-	WorkerWriteTimeout    time.Duration `env:"WORKER_WRITE_TIMEOUT"     env-required:"false" env-default:"3s"`
-	WorkerShutdownTimeout time.Duration `env:"WORKER_SHUTDOWN_TIMEOUT"  env-required:"false" env-default:"0s"`
+	WorkerHost            string        `env:"WORKER_HOST"              env-default:"localhost"`
+	WorkerReadTimeout     time.Duration `env:"WORKER_READ_TIMEOUT"      env-default:"3s"`
+	WorkerWriteTimeout    time.Duration `env:"WORKER_WRITE_TIMEOUT"     env-default:"3s"`
+	WorkerShutdownTimeout time.Duration `env:"WORKER_SHUTDOWN_TIMEOUT"  env-default:"3s"`
 	// time for worker node to set up
-	WorkerSetupDur time.Duration `env:"WORKER_SETUP_DURATION" env-required:"false" env-default:"10s"`
+	WorkerSetupDur time.Duration `env:"WORKER_SETUP_DURATION" env-default:"10s"`
 	// time for worker to receive a new job
-	WorkerAwaitDur time.Duration `env:"WORKER_AWAIT_DURATION" env-required:"false" env-default:"10s"`
+	WorkerAwaitDur time.Duration `env:"WORKER_AWAIT_DURATION" env-default:"10s"`
 
 	// ================== RABBITMQ ==================
-	RabbitLogin        string `env:"RABBIT_LOGIN"         env-required:"false" env-default:"login"`
-	RabbitPassword     string `env:"RABBIT_PASSWORD"      env-required:"false" env-defualt:"password"`
-	RabbitHost         string `env:"RABBIT_HOST"          env-required:"false" env-default:"host"`
-	RabbitPort         string `env:"RABBIT_PORT"          env-required:"false" env-default:"port"`
-	RabbitMapperQueue  string `env:"RABBIT_MAPPER_QUEUE"  env-required:"false" env-default:"mapper"`
-	RabbitReducerQueue string `env:"RABBIT_REDUCER_QUEUE" env-required:"false" env-default:"reducer"`
+	RabbitLogin        string `env:"RABBIT_LOGIN"         env-default:"login"`
+	RabbitPassword     string `env:"RABBIT_PASSWORD"      env-defualt:"password"`
+	RabbitHost         string `env:"RABBIT_HOST"          env-default:"localhost"`
+	RabbitPort         string `env:"RABBIT_PORT"          env-default:"5673"`
+	RabbitMapperQueue  string `env:"RABBIT_MAPPER_QUEUE"  env-default:"mapper"`
+	RabbitReducerQueue string `env:"RABBIT_REDUCER_QUEUE" env-default:"reducer"`
 }
 
 func NewConfig(envLocation string) (*Config, error) {
